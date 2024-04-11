51569c1e9e84bb481b9c711253d1a07067023833
Dan Robin Eriksen <dan@dx.no>
2024-04-11 15:34:29 +0200

    first commit

---
+++ MauiIntellisenseBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiIntellisenseBug;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MauiIntellisenseBug/Platforms/Android/MainApplication.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Runtime;
+using Com.Sam4s.Printer;
+
+namespace MauiIntellisenseBug;
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+        var thisIsThirteen = Sam4sBuilder.LangKo;
+
+        // Set a breakpoint here, see that the value of thisIsThirteen has been set to 13
+        var breakpointHere = true;
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
